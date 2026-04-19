e281aafe293bbd7bd8f327d6926d9ceffe6bb705
Dubhe-Giliandor <grimaldigaming@gmail.com>
2026-04-19 14:39:22 +0200

    First Change

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ function App() {
       <h1>Hello from VS Code 🚀</h1>
       <p>This is my JSX demo project.</p>
       <button onClick={() => alert("It works!")}>
-        Click me
+        Fuck off
       </button>
     </div>
   );
