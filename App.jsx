df5099603a1cb8e00e7a7ba4bae477361c4509e6
Dubhe-Giliandor <grimaldigaming@gmail.com>
2026-04-19 14:33:52 +0200

    Initial Commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from VS Code 🚀</h1>
+      <p>This is my JSX demo project.</p>
+      <button onClick={() => alert("It works!")}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+import App from "./App.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
